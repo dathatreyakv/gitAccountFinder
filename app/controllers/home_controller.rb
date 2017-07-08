c759bf1918f6ec2ae4d6b2ff2ae8ebcd8d52f0ce
dathuk <dathu.k@mdoffice.in>
2017-07-08 15:54:36 +0530

    basic UI related changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,30 +1,32 @@
 class HomeController < ApplicationController
+  
   def index
+
   end
 
   def search
-	@git_user = GitAccount.where(user_name: user_name.downcase).first
+  	if user_name.present?
+		@git_user = GitAccount.where(user_name: user_name.downcase).first
 
-	if @git_user.present?
-		
-	else
-		details = user_details
-		if details["message"].present?
-			error_message = "'#{user_name}' not exist in Gitlab "
+		if @git_user.present?
+			@repositories = Repository.where(git_account_id: @git_user.id)
 		else
-			repositories = user_repositories
-			binding.pry
-			@git_user = GitAccount.create(user_name: user_name.downcase, user_id: details["id"], profile_url: details["html_url"], location: details["location"])
-			_rep = []
-			binding.pry
-			repositories.each do |repository|
-				_rep << Repository.new( repository_url: repository["html_url"] )
+			details = user_details
+			if details["message"].present?
+				@error_message = "'#{user_name}' not exist in Gitlab "
+			else
+				repositories = user_repositories
+				@git_user = GitAccount.create(user_name: user_name.downcase, user_id: details["id"], profile_url: details["html_url"], location: details["location"])
+				_rep = []
+				repositories.each do |repository|
+					_rep << Repository.new( repository_url: repository["html_url"] )
+				end
+				@git_user.repositories << _rep if _rep.present?
+				@repositories = Repository.where(git_account_id: @git_user.id)
 			end
-			@git_user.repositories << _rep if _rep.present?
-			binding.pry
 		end
 	end
-
+	render :index
   end
 
   private
