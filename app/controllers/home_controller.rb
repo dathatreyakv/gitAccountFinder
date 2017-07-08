777a4342ff8af94758087cd42939cd15ad57d983
dathuk <dathu.k@mdoffice.in>
2017-07-08 15:28:07 +0530

    business logic changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,46 +3,44 @@ class HomeController < ApplicationController
   end
 
   def search
-  	binding.pry
-	@git_user = GitAccount.where(user_name: search_by.downcase).first
+	@git_user = GitAccount.where(user_name: user_name.downcase).first
 
 	if @git_user.present?
-		binding.pry
 		
 	else
-		inforamtion = user_detail
+		details = user_details
+		if details["message"].present?
+			error_message = "'#{user_name}' not exist in Gitlab "
+		else
+			repositories = user_repositories
+			binding.pry
+			@git_user = GitAccount.create(user_name: user_name.downcase, user_id: details["id"], profile_url: details["html_url"], location: details["location"])
+			_rep = []
+			binding.pry
+			repositories.each do |repository|
+				_rep << Repository.new( repository_url: repository["html_url"] )
+			end
+			@git_user.repositories << _rep if _rep.present?
+			binding.pry
+		end
 	end
 
   end
 
   private
 
-  def user_detail
-    binding.pry
-    uri = URI.parse("https://api.github.com/users/"+search_by)
-    # header = {"Content-Type"=> "application/json"}
-    conn = Net::HTTP.new(uri.host, uri.port)
-    req = Net::HTTP::Get.new(uri.request_uri)
-    res = conn.request(req)
-    binding.pry
-    res.body
-    # Nokogiri::XML res.body
-    # binding.pry
-  end
+	def user_details
+		result = HTTParty.get("https://api.github.com/users/#{user_name}")
+		JSON.parse(result.response.body)
+	end
 
-  def user_repositories
-    uri = URI.parse("https://api.github.com/users/"+search_by+"/repos")
-    #header = {"Content-Type"=> "application/json"}
-    conn = Net::HTTP.new(uri.host, uri.port)
-    req = Net::HTTP::Get.new(uri.request_uri)
-    res = conn.request(req)
-    res.body
-    # Nokogiri::XML res.body
-    # binding.pry
-  end
+	def user_repositories
+		result = HTTParty.get("https://api.github.com/users/#{user_name}/repos")
+		JSON.parse(result.response.body)
+	end
 
-  def search_by
-  	(params.permit!)[:user_name]
-  end
+	def user_name
+		(params.permit!)[:user_name]
+	end
 
 end
